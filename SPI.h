100a3cfcaf32951f60882a5207cfe403087c1e85
Roy Bakker <r.bakker@hr.nl>
2019-04-07 18:43:46 +0000

    SPI.h edited online with Bitbucket --> Fixed possible initialization bug

--- SPI.h
+++ SPI.h
@@ -10,16 +10,16 @@ inline static void SPI_end();
 
 void SPI_begin(void) {
     /* Configure P1.5, 1.6, 1.7 for SPI. */
-    P1SEL = BIT5 + BIT6 + BIT7;
-    P1SEL2 = BIT5 + BIT6 + BIT7;
+    P1SEL |= (BIT5 | BIT6 | BIT7);
+    P1SEL2 |= (BIT5 | BIT6 | BIT7);
 
     /* Setup SPI on USCI B */
-    UCB0CTL1 = UCSSEL_2 + UCSWRST;               // SMCLK
-    UCB0CTL0 = UCCKPL + UCMSB + UCMST + UCSYNC;  // 3-pin, 8-bit SPI master
-    UCB0BR0 = 8;                                 // /8
-    UCB0BR1 = 0;                                 //
+    UCB0CTL1 = (UCSSEL_2 | UCSWRST);               // SMCLK + Reset
+    UCB0CTL0 = (UCCKPL | UCMSB | UCMST | UCSYNC);  // 3-pin, 8-bit SPI master
+    UCB0BR0 = 8;                                   // /8
+    UCB0BR1 = 0;                                   //
 
-    UCB0CTL1 &= ~UCSWRST;                        // release USCI for operation
+    UCB0CTL1 &= ~UCSWRST;                          // release USCI for operation
 }
 
 uint8_t SPI_transfer(uint8_t data) {
